70f438086cb0b60fe083aa5dbe827882e05a1fe6
Munei Nengwenani <>
2019-01-06 11:05:38 +0200

    added scss support

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDom from "react-dom"
 import "@babel/polyfill"
 import App from "./App"
-import "./styles.css"
+import "./styles.scss"
 import DefaultErrorBoundry from "./DefaultErrorBoundry"
 
 ReactDom.render(
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,31 +1,33 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'main.bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader:'babel-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test:/\.css$/,
-                use: ['style-loader', 'css-loader'],
-                exclude: /node_modules/
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
-    
-      
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "main.bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
 }
