f655614071db05553a4746c2eca7beb0ae84fda4
Munei Nengwenani <>
2019-01-06 10:21:31 +0200

    Added defaultErrorBoundry file

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDom from "react-dom"
 import "@babel/polyfill"
 import App from "./App"
 import "./styles.css"
+import DefaultErrorBoundry from "./DefaultErrorBoundry"
 
 ReactDom.render(
   <React.StrictMode>
-    <App />
+    <DefaultErrorBoundry>
+      <App />
+    </DefaultErrorBoundry>
   </React.StrictMode>,
   document.getElementById("app")
 )
